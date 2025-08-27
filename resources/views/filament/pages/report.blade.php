09ab834d49ec44b750cf6147e00e89723dd56629
Ahmad Affandi <caspianp98@yahoo.com>
2025-08-27 13:21:05 +0700

    fitur : tambahan laporan berdasarkan layanan

--- resources/views/filament/pages/report.blade.php
+++ resources/views/filament/pages/report.blade.php
@@ -2,12 +2,13 @@
     <div class="!mt-[0px]">
         <!-- Tabs -->
         <div class="mb-4 border-b border-gray-200">
-            <nav class="-mb-px flex space-x-8">
+            <nav class="-mb-px flex space-x-8 overflow-auto">
                 <button wire:click="$set('activeTab', 1)" class="{{ $activeTab === 1 ? 'border-indigo-500 text-indigo-600' : 'border-transparent text-gray-500' }} whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm">{{ __('Trend Ticket') }}</button>
                 <button wire:click="$set('activeTab', 2)" class="{{ $activeTab === 2 ? 'border-indigo-500 text-indigo-600' : 'border-transparent text-gray-500' }} whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm">{{ __('Ticket by owner') }}</button>
                 <button wire:click="$set('activeTab', 3)" class="{{ $activeTab === 3 ? 'border-indigo-500 text-indigo-600' : 'border-transparent text-gray-500' }} whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm">{{ __('Ticket by responsible') }}</button>
-                <button wire:click="$set('activeTab', 4)" class="{{ $activeTab === 4 ? 'border-indigo-500 text-indigo-600' : 'border-transparent text-gray-500' }} whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm">{{ __('Ticket by application') }}</button>
-                <button wire:click="$set('activeTab', 5)" class="{{ $activeTab === 5 ? 'border-indigo-500 text-indigo-600' : 'border-transparent text-gray-500' }} whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm">{{ __('Ticket duplicates') }}</button>
+                <button wire:click="$set('activeTab', 4)" class="{{ $activeTab === 4 ? 'border-indigo-500 text-indigo-600' : 'border-transparent text-gray-500' }} whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm">{{ __('Ticket by service') }}</button>
+                <button wire:click="$set('activeTab', 5)" class="{{ $activeTab === 5 ? 'border-indigo-500 text-indigo-600' : 'border-transparent text-gray-500' }} whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm">{{ __('Ticket by application') }}</button>
+                <button wire:click="$set('activeTab', 6)" class="{{ $activeTab === 6 ? 'border-indigo-500 text-indigo-600' : 'border-transparent text-gray-500' }} whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm">{{ __('Ticket duplicates') }}</button>
             </nav>
         </div>
 
@@ -20,8 +21,10 @@
             @elseif($activeTab === 3)
                 @livewire(\App\Filament\Widgets\TicketResponsibilityChart::class, [], key('ticket-responsibility'))
             @elseif($activeTab === 4)
-                @livewire(\App\Filament\Widgets\TicketByApplicationChart::class, [], key('ticket-by-application'))
+                @livewire(\App\Filament\Widgets\TicketByServiceChart::class, [], key('ticket-by-service'))
             @elseif($activeTab === 5)
+                @livewire(\App\Filament\Widgets\TicketByApplicationChart::class, [], key('ticket-duplicate'))
+            @elseif($activeTab === 6)
                 @livewire(\App\Filament\Widgets\TicketDuplicateChart::class, [], key('ticket-duplicate'))
             @endif
         </div>
